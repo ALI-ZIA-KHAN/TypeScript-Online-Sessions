3f20ca797ad2cd9c5ea9c2d068708cef5161f077
ALI-ZIA-KHAN <azk443.443@gmail.com>
2023-03-24 01:08:36 +0500

    Class 1 code

---
+++ Class-1-23-03-23/app.ts
@@ -0,0 +1,46 @@
+// whatever u r getting in by getElemnt in imput1 else.g it can be empty or even don't exist
+// so typecript wants assurity that it actually exists so  u give ! as HTMLInputElement after that
+
+// const input1 = document.getElementById("num1")! as HTMLInputElement;
+// const input2 = document.getElementById("num2")! as HTMLInputElement;
+// const button = document.getElementById("btn")!;
+
+
+
+//if have simply run add function as js it would have concatenate instead off adding as .value gives string by def
+//by giving types now it will add the numbers
+// const add = function (val1: number, val2: number) {
+//     return val1 + val2;
+// };
+
+// button.addEventListener("click", function () {
+//     const firstVal = input1.value;
+//     const secondVal = input2.value;
+//     console.log(add(+firstVal, +secondVal));
+// });
+//using +with arg is must to make it number as given data type in parameter
+
+
+
+// Basic Types
+
+// 1. String: string => "Apple", "Orange"
+// 2. Number: number => 1 0 -87 4.5
+// 3. Boolean: boolean => true false
+
+let person: number;
+
+person = 8;
+
+let arr: string[];
+
+arr = ["Apple", "Orange", ];
+arr.push("Pineapple");
+arr.push();
+
+const arr2: number[] = [1, 2, 3];
+arr2.push();
+
+
+// This is my new comment.
+// when u add any code comile it again so u will see updated js file
